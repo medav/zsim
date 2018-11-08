dffbba198951c015507040324e3ec3afdff01330
Michael Davies <davies@cs.wisc.edu>
2018-11-08 14:50:31 -0600

    Update naive matmul to generate its own matrices

--- apps/matmul/naive-matmul.cpp
+++ apps/matmul/naive-matmul.cpp
@@ -4,28 +4,38 @@
 #include <stdio.h>
 #include <cstring>
 #include <vector>
+#include <string.h>
+#include <stdio.h>
 #include <condition_variable>
 
 #include <pthread.h>
 
-#define NUMTILES 64
-#define MATSIZE 20
+inline int MatIndex(int mat_size, int row, int col) {
+    return row * mat_size + col;
+}
 
 typedef struct _global_data {
-    int mat_a[MATSIZE][MATSIZE];
-    int mat_b[MATSIZE][MATSIZE];
-    int output[MATSIZE][MATSIZE];
+    int mat_size;
+    int num_cores;
+    int * mat_a;
+    int * mat_b;
+    int * mat_o;
 } global_data;
 
 static inline void compute_element(
-    int mat_a[MATSIZE][MATSIZE],
-    int mat_b[MATSIZE][MATSIZE],
-    int output[MATSIZE][MATSIZE],
+    global_data * gd,
     int row,
     int col)
 {
-    for (int i = 0; i < MATSIZE; i++) {
-        output[row][col] += mat_a[row][i] * mat_b[i][col];
+    int * mat_a = gd->mat_a;
+    int * mat_b = gd->mat_b;
+    int * mat_o = gd->mat_o;
+
+    for (int i = 0; i < gd->mat_size; i++) {
+
+        mat_o[MatIndex(gd->mat_size, row, col)] +=
+            mat_a[MatIndex(gd->mat_size, row, i)] *
+            mat_b[MatIndex(gd->mat_size, i, col)];
     }
 }
 
@@ -34,11 +44,11 @@ void worker(int id, global_data * gd) {
     int row;
     int col;
 
-    for (id; id < MATSIZE * MATSIZE; id += NUMTILES) {
-        row = id / MATSIZE;
-        col = id % MATSIZE;
+    for (id; id < gd->mat_size * gd->mat_size; id += gd->num_cores) {
+        row = id / gd->mat_size;
+        col = id % gd->mat_size;
 
-        compute_element(gd->mat_a, gd->mat_b, gd->output, row, col);
+        compute_element(gd, row, col);
     }
 }
 
@@ -51,30 +61,30 @@ void worker_entry(int id, global_data * gd) {
     worker(id, gd);
 }
 
-void read_csv(int dest[MATSIZE][MATSIZE], const char * filename) {
-    int row;
-    int col;
-    FILE * f = fopen(filename, "r");
-
-    for (int i = 0; i < MATSIZE * MATSIZE; i++) {
-        row = i / MATSIZE;
-        col = i % MATSIZE;
-
-        fscanf(f, "%d ", &dest[row][col]);
+void gen_csv(int * dest, const size_t mat_size) {
+    for (size_t i = 0; i < mat_size * mat_size; i++) {
+        dest[i] = rand() % 100;
     }
-
-    fclose(f);
 }
 
-int main() {
-    global_data * gd = new global_data;
+int main(int argc, char * argv[]) {
+    int num_cores = atoi(argv[1]);
+    int mat_size = atoi(argv[2]);
+
+    global_data gd;
+    gd.num_cores = num_cores;
+    gd.mat_size = mat_size;
+    gd.mat_a = new int[mat_size * mat_size];
+    gd.mat_b = new int[mat_size * mat_size];
+    gd.mat_o = new int[mat_size * mat_size];
+
     std::vector<std::thread> threads;
 
     printf("Reading matrices\n");
-    read_csv(gd->mat_a, "apps/matmul/mat_a.csv");
-    read_csv(gd->mat_b, "apps/matmul/mat_b.csv");
+    gen_csv(gd.mat_a, mat_size);
+    gen_csv(gd.mat_b, mat_size);
 
-    memset(gd->output, 0x00, MATSIZE * MATSIZE * sizeof(int));
+    memset(gd.mat_o, 0x00, mat_size * mat_size * sizeof(int));
 
     printf("Setting affinity for thread 0\n");
     cpu_set_t cpuset;
@@ -83,26 +93,28 @@ int main() {
     int rc = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 
     printf("Spawning worker threads\n");
-    for (int id = 1; id < NUMTILES; id++) {
-        threads.push_back(std::thread(worker, id, gd));
+    for (int id = 1; id < num_cores; id++) {
+        threads.push_back(std::thread(worker, id, &gd));
     }
 
     printf("Working...\n");
-    worker(0, gd);
+    worker(0, &gd);
 
     printf("Waiting for threads...\n");
-    for (int id = 0; id < NUMTILES - 1; id++) {
+    for (int id = 0; id < num_cores - 1; id++) {
         threads[id].join();
     }
 
-    for (int row = 0; row < MATSIZE; row++) {
-        for (int col = 0; col < MATSIZE; col++) {
-            printf("%d, ", gd->output[row][col]);
+    for (int row = 0; row < mat_size; row++) {
+        for (int col = 0; col < mat_size; col++) {
+            printf("%d, ", gd.mat_o[MatIndex(mat_size, row, col)]);
         }
         printf("\n");
     }
 
-    delete gd;
+    delete gd.mat_a;
+    delete gd.mat_b;
+    delete gd.mat_o;
 
     return 0;
 }
