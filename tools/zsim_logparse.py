9d32819170f0ec60dedc4600c97110550b3efe36
gauravjain14 <41287729+gauravjain14@users.noreply.github.com>
2018-11-11 18:11:20 -0600

    Update zsim_logparse.py

--- tools/zsim_logparse.py
+++ tools/zsim_logparse.py
@@ -4,7 +4,6 @@ import re
 core_regex = re.compile(r'c-(\d+)')
 
 def MaxCoreCycles(outlog):
-<<<<<<< HEAD
     try:
         cur_core = -1
         cycle_counts = []
@@ -26,26 +25,6 @@ def MaxCoreCycles(outlog):
         return max(cycle_counts)
     except:
         return None
-=======
-    cur_core = -1
-    cycle_counts = []
-
-    with open(outlog, 'r') as f:
-        for line in f:
-            if (not line.startswith('   ')) and (cur_core != -1):
-                cur_core = -1
-
-            if line.startswith('  '):
-                m = core_regex.search(line)
-                if m is not None:
-                    cur_core = int(m.group(1))
-
-            if cur_core != -1:
-                if '   cycles' in line:
-                    cycle_counts.append(int(line.split()[1]))
-
-    return max(cycle_counts)
->>>>>>> 98e75acb344a7db9dec7c3777bbe22cd94079d32
 
 def AvgContCycles(outlog):
     return 0
